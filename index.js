84d7187b21fb1327779a0170e329371acac08cdb
rahat2020 <kazimdboktiar2020@gmail.com>
2022-04-30 02:24:48 +0600

    bug fixing

--- index.js
+++ index.js
@@ -34,12 +34,8 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/images", express.static(path.join(__dirname,"./images")))
 
 // the api
-const corsOptions ={
-    origin:'http://localhost:5000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
