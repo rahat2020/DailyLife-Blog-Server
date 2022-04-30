852cc52af47e982f8ce91ed0dfd6715a0de0610d
rahat2020 <kazimdboktiar2020@gmail.com>
2022-04-30 22:33:26 +0600

    fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 
@@ -17,6 +17,7 @@ mongoose.connect(MONGO_URL)
     .then(() => console.log('database connection established'))
     .catch(err => console.log('error connecting', err))
     
+app.use(cors());
 // UPLOADING IMAGES TO THE SERVER
 const storage = multer.diskStorage({
     destination: (req, res, cb) => {
@@ -34,15 +35,6 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/images", express.static(path.join(__dirname,"./images")))
 
 // the api
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
-// app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
