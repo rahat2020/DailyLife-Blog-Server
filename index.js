a382eb69bcb8a246b15f1c4d62e3078448c643e5
rahat2020 <kazimdboktiar2020@gmail.com>
2022-04-30 11:36:07 +0600

    bug fixing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 
@@ -34,8 +34,15 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/images", express.static(path.join(__dirname,"./images")))
 
 // the api
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-app.use(cors());
+app.use(cors(corsOptions))
+// app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
